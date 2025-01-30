b332ac9c8224bbbff20f31bfbf15ce60f5e87c9b
unknown <hp@LAPTOP-Q9P184RA>
2025-01-30 15:43:58 +0530

    Updated/created/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -5,6 +5,10 @@ public class LoginPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside LP");
+		String name ="admin";
+		String account ="admin";
+		
+		System.out.println("login to admin");
 	}
 
 }
--- src/test/TestingLogin.java
+++ src/test/TestingLogin.java
@@ -1,11 +1,10 @@
 package test;
 
-public class TestClass {
+public class TestingLogin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Inside Test Class");
 	}
 
 }
